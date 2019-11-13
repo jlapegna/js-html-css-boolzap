bbae03b57a9bd74c6c07f8011d914085d410ccf0
Jolanda <jlapegna@outlook.it>
2019-11-13 21:08:02 +0100

    milestone 2 - i stil need to fix up the framework a bit since some of the rows are off

--- assets/js/master.js
+++ assets/js/master.js
@@ -11,24 +11,38 @@ $(document).ready(function() {
     // We make a copy of an empty Div + we add a class with our css in it
     var elementMsg = $("#template .msgsent").clone().addClass('msgsent-style');
 
-    // We add the user's message to that empty Div
-
-    elementMsg.find(".testo").text(userText);
-
-    // We add out element containing the User's message to our HTML
-
-    $(".message-screen").append(elementMsg);
-
-    // This passes an empty string into the message input to clear it
-
-    $(".message").val("");
-
-// Current Time
+    // Current Time
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes();
 
-    console.log(time);
+    // We add the user's message to that empty Div + the current time
+    elementMsg.find(".testo").text(userText + " " + time);
 
+    // We add out element containing the User's message to our HTML
+    $(".message-screen").append(elementMsg);
+
+    // This passes an empty string into the message input to clear it
+    $(".message").val("");
+
+    // Response from PC
+
+    // Create a clone of the empty div we want to paste the reply into
+    var elementMsgg = $("#template .msgsent").clone().addClass('msgsent-style');
+
+    // Functions
+
+    // Function that creates an automatic response
+    setTimeout(risponse, 1000);
+
+    function risponse() {
+      var okMessage = 'ok';
+
+      var messageResponse = $("#template .msgsent").clone().addClass('msgrecieved-style');
+
+      messageResponse.find(".testo").text(okMessage);
+
+      $('.message-screen').append(messageResponse);
+    }
 
   })
 
