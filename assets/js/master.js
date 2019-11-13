23687fc46df7bd3dd1971be0cc627bf99938aac2
Jolanda <jlapegna@outlook.it>
2019-11-13 15:38:09 +0100

    finished the first milestone JS, just need to fix up the containers a bit

---
+++ assets/js/master.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+
+// When the airplance icon (send button) is clicked the function is called
+
+  $(".submit-button").click(function(){
+
+    // We save whatever the user wants to send as a message
+    var userText = $(".message").val();
+    console.log(userText);
+
+    // We make a copy of an empty Div + we add a class with our css in it
+    var elementMsg = $("#template .msgsent").clone().addClass('msgsent-style');
+
+    // We add the user's message to that empty Div
+
+    elementMsg.find(".testo").text(userText);
+
+    // We add out element containing the User's message to our HTML
+
+    $(".message-screen").append(elementMsg);
+
+    // This passes an empty string into the message input to clear it
+
+    $(".message").val("");
+
+
+  })
+
+});
