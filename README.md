eccd8b651e401a4a3a34803d6ea51676a14777e8
jolanda <thelapegna@gmail.com>
2020-05-13 16:06:52 +0200

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-js-html-css-boolzap
+# "Boolzap" A WhatsApp Clone
+
+## Goal
+
+Recreate a copy of the WhatsApp web app with JQuery. The project will be completed in three milestones.
+
+![alt text](https://github.com/jlapegna/js-pokedex-api/blob/master/img/milestones.png?raw=true)
+
+
+## Key Concepts
+
+* HTML
+* CSS
+* Layout design with Flexbox
+* Javascript
+* JQuery
--- assets/js/master.js
+++ assets/js/master.js
@@ -1,3 +1,8 @@
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messagg
+
+
 $(document).ready(function() {
 
   // When the airplance icon (send button) is clicked the function is called
@@ -6,7 +11,6 @@ $(document).ready(function() {
 
     // We save whatever the user wants to send as a message
     var userText = $(".message").val();
-    console.log(userText);
 
     // We make a copy of an empty Div + we add a class with our css in it
     var elementMsg = $("#template .msgsent").clone().addClass('msgsent-style');
@@ -24,7 +28,9 @@ $(document).ready(function() {
     // This passes an empty string into the message input to clear it
     $(".message").val("");
 
-    // Response from PC
+
+    // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
 
     // Create a clone of the empty div we want to paste the reply into
     var elementMsgg = $("#template .msgsent").clone().addClass('msgsent-style');
@@ -39,11 +45,39 @@ $(document).ready(function() {
 
       var messageResponse = $("#template .msgsent").clone().addClass('msgrecieved-style');
 
-      messageResponse.find(".testo").text(okMessage);
+      messageResponse.find(".testo").text(okMessage + " " + time);
 
       $('.message-screen').append(messageResponse);
     }
 
   })
+
+
+  // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+
+  $('#contacts-filter').keyup(function(event){
+
+    var userSearch = $(this).val().toLowerCase();
+
+    $('.contact').each(function(){
+      var contactNames = $(this).find('.name').text().toLowerCase();
+
+      if (contactNames.includes(userSearch)){
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    })
+
+  })
+
+//  Milestone 3
+// Click sul contatto mostra la conversazione del contatto cliccato, è possibile inserire nuovi messaggi per ogni conversazione
+
+
+
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+
 
 });
